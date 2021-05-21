c64592fd9d34503a890df1cbd9689e4bd569e9e2
Daniel Finke <danielfinke2011@gmail.com>
2021-05-21 15:20:13 -0700

    Multiple dispatch calls in one thunk

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,5 @@
 export const ADD_TWEETS = 'ADD_TWEETS';
+export const SET_STATUS = 'SET_STATUS';
 
 export const addTweets = (tweets) => ({
   type: ADD_TWEETS,
@@ -8,9 +9,21 @@ export const addTweets = (tweets) => ({
 });
 
 export const fetchTweets = () => (dispatch) => {
+  dispatch(setStatus('loading'));
   fetch('http://tweet-stream.glitch.me/api/tweets')
     .then((response) => response.json())
     .then((response) => {
       dispatch(addTweets(response.tweets));
+      dispatch(setStatus('loaded'));
+    })
+    .catch(() => {
+      dispatch(setStatus('error'));
     });
 };
+
+export const setStatus = (status) => ({
+  type: SET_STATUS,
+  payload: {
+    status,
+  },
+});
